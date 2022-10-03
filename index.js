8c7e90c2e611de24689543ba63f3bd00de0f1a7a
Ghaithkhaled <212622@vistacollege.nl>
2022-10-03 09:27:36 +0200

    header

---
+++ index.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('js-rotate');
+const button = document.getElementById('js-menu-button');
+const hamburger = document.getElementById('js-hamburger');
+button.onclick = (e) => {
+  e.preventDefault;
+  header.classList.toggle('main-header--rotate');
+  hamburger.classList.toggle('menu-button__hb--is-active');
+}

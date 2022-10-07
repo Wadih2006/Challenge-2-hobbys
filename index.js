aa9e3460318c80f80b5fcff57e189129a8f6361c
Ghaithkhaled <212622@vistacollege.nl>
2022-10-07 09:18:03 +0200

    test

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ const header = document.getElementById('js-rotate');
     e.preventDefault;
     header.classList.toggle('main-header--rotate');
     hamburger.classList.toggle('menu-button__hb-is-active');}
+
+
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        for(let i = 0; i < 75; i++) {
+          let bubble = document.createElement('div');
+          bubble.className = 'bubble';
+          
+          bubble.style.cssText = `
+            --blob-size: ${2 + Math.random()*4}rem;
+            --blob-distance: ${5 + Math.random()*4}rem;
+            --blob-position: ${-5 + Math.random()*110}%;
+            --blob-time: ${2 + Math.random()*2}s;
+            --blob-delay: ${-1 * (2 + Math.random()*2)}s;
+          `;
+          
+          document.querySelector('#bubble-container').append(bubble);
+        }
+      });
